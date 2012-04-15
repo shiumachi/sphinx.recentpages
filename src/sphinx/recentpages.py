7ea4615aafd5b4301bba496458458ab3ffdc864d
Sho Shimauchi <sho.shimauchi@gmail.com>
2012-04-15 23:18:53 +0900

    add new method: RecentPages.getSortedFileListWithMtime()

--- src/sphinx/recentpages.py
+++ src/sphinx/recentpages.py
@@ -1,6 +1,51 @@
 # -*- coding: utf-8 -*-
+"""
+    sphinx.recentpages
+    ~~~~~~~~~~~~~~~~~~
+
+    :copyright: Copyright 2012 by Sho Shimauchi.
+    :license: BSD, see LICENSE for details.
+"""
+
+import os
 
 class RecentPages(object):
+
+    @classmethod
+    def getSortedFileListWithMtime(self, target_dir):
+        """get sorted file lists in specified directory.
+
+        Args:
+        target_dir: target directory to get all file lists.
+
+        Returns:
+        list of files ordered by mtime.
+        """
+        
+        res = []
+        
+        fileList = self._walk(target_dir)        
+        for file in fileList:
+            res.append((file, int(os.stat(file).st_mtime)))
+                       
+        res.sort(cmp=lambda x,y: cmp(x[1],y[1]))
+        return res
+
+    @classmethod
+    def _walk(self, dir):
+        res = []
+        if dir == "": return res
+        for w in os.walk(dir):
+            rel_path, dir_list, file_list = w
+            for f in file_list:
+                res.append(rel_path + "/" + f)
+            for d in dir_list:
+                res += self._walk(d)
+        return res
+            
+
+        
+        
     
     @classmethod
     def main(self):
--- src/test/sphinx/TestRecentPages.py
+++ src/test/sphinx/TestRecentPages.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 from sphinx.recentpages import RecentPages
+from test.sphinx.Constants import Constants
 from nose.tools import *
 import unittest
 import sys
+import os
+
 
 class TestRecentPages(unittest.TestCase):
+    target_dir = Constants.TEST_ROOT_DIR
+
+    def testGetSortedFileListWithMtime(self):
+        expected = [
+            (self.target_dir + "/test001", int(os.stat(self.target_dir + "/test001").st_mtime)),
+            (self.target_dir + "/test002", int(os.stat(self.target_dir + "/test002").st_mtime)),
+            (self.target_dir + "/test003", int(os.stat(self.target_dir + "/test003").st_mtime)),
+            (self.target_dir + "/dir01/test004", int(os.stat(self.target_dir + "/dir01/test004").st_mtime)),
+            (self.target_dir + "/dir02/dir03/test005", int(os.stat(self.target_dir + "/dir02/dir03/test005").st_mtime)),
+            (self.target_dir + "/test006", int(os.stat(self.target_dir + "/test006").st_mtime)),
+            ]
+        assert expected == RecentPages.getSortedFileListWithMtime(self.target_dir)
 
     def testMain(self):
         RecentPages.main()
