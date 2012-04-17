ee5d25e08580a863a29d16a49753a9b33b316b53
Sho Shimauchi <sho.shimauchi@gmail.com>
2012-04-17 17:07:43 +0900

    change manifest and update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sphinx.recentpages',
-      version='0.1',
+      version='0.2',
       author='Sho Shimauchi',
       author_email='sho.shimauchi@gmail.com',
       test_suite='nose.collector',
