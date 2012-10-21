0c661bcd3b136bc7f52b4856df2035d88abbe28f
Sho Shimauchi <sho.shimauchi@gmail.com>
2012-10-22 01:21:49 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='sphinx.recentpages',
-      version='0.3',
+      version='0.4',
       author='Sho Shimauchi',
       author_email='sho.shimauchi@gmail.com',
       test_suite='nose.collector',
       tests_require='Nose',
       packages=find_packages('sphinxcontrib'),
-      package_dir={'' : 'sphinxcontrib-recentpages' },
+      package_dir={'' : 'sphinxcontrib' },
       )
